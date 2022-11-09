2b422833e1ffe708af91de5bdc5aabdcf8f8bc8b
GameUnmaker <damastazen@gmail.com>
2022-11-09 04:21:54 -0500

    Email
    That's right dudes, we can spam emails now.

---
+++ Inventory Application/src/main/java/com/inventory/inventory/SendEmail.java
@@ -0,0 +1,47 @@
+package com.inventory.inventory;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendEmail {
+
+    public static void sendMail(String recepient, String subjectMsg, String subjectText) throws Exception {
+        System.out.println("Attempting to send email...");
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host","smtp.office365.com");
+        properties.put("mail.smtp.port", "587");
+
+        String myAccountEmail = "biyinventoryapplication@hotmail.com";
+        String password = "ThisGirlIsOnFire1!1!";
+
+        Session session = Session.getInstance(properties, new Authenticator(){
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+        Message message = prepareMessage(session, myAccountEmail, recepient, subjectMsg, subjectText);
+
+        Transport.send(message);
+        System.out.println("Success! Probably.");
+    }
+
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient, String subjectMsg, String subjectText){
+        try{
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject(subjectMsg);
+            message.setText(subjectText);
+            return message;
+        } catch (Exception ex) {
+            System.out.println("Well I hope this doesn't break shit");
+        }
+        return null;
+    }
+}
--- Inventory Application/src/main/java/module-info.java
+++ Inventory Application/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.inventory.inventory {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires java.sql;
+    requires java.mail;
 
     opens com.inventory.inventory to javafx.fxml;
     exports com.inventory.inventory;
