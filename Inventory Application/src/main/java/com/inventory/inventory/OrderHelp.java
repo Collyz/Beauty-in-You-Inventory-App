7a72c92d4f6dcc0fccd7e0ebf8e80b9669f53218
CybErr404 <114787759+CybErr404@users.noreply.github.com>
2022-11-23 10:28:39 -0500

    Update OrderHelp.java

--- Inventory Application/src/main/java/com/inventory/inventory/OrderHelp.java
+++ Inventory Application/src/main/java/com/inventory/inventory/OrderHelp.java
@@ -1,4 +1,15 @@
 package com.inventory.inventory;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class OrderHelp {
 
+    @FXML private Text editOrderHelp = new Text();
+    @FXML
+    public void onExit() {
+        Stage stage = (Stage) editOrderHelp.getScene().getWindow();
+        stage.close();
+    }
 }
